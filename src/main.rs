fd4369dbd0171a0261387c502345e37777f71f74
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-05 22:23:11 +0200

    Add extracting specific args

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,10 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Provided query: {}", query);
+    println!("Filename to search in: {}", filename);
 }

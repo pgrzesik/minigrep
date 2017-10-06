8b381a8a4f3371b4ac715ebdf5dc33e96b7f9100
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-06 23:04:19 +0200

    Refactor implementation to lib.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+extern crate minigrep;
+
 use std::env;
 use std::process;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,37 +13,8 @@ fn main() {
         process::exit(1)
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("During application execution, following error occured: {}", e);
         process::exit(1)
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("File content:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments provided!")
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }

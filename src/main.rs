9dfad2406bb6815b5c629773465c7a09a88ce062
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-06 23:29:12 +0200

    Write errors to stderr instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("During parsing arguments, experienced error: {}", err);
+        eprintln!("During parsing arguments, experienced error: {}", err);
         process::exit(1)
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("During application execution, following error occured: {}", e);
+        eprintln!("During application execution, following error occured: {}", e);
         process::exit(1)
     }
 }

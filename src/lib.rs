8a81e23158d83762dff47a5d6d4cfcdc1881eaf2
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-08 21:48:07 +0200

    Reimplement search with iterators

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@ pub struct Config {
 
 impl Config {
     pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
-        args.next(); // Skip first arg (executable)
+        args.next(); // Skip first arg (executable name)
 
         let query = match args.next() {
             Some(arg) => arg,
@@ -50,15 +50,9 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
